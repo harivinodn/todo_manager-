61445692116583c7a939897d349a7bfffab06194
harivinodn <harivinodn@gmail.com>
2021-10-22 15:24:44 +0530

    Added sign-in feature without session

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+
+  def new
+    render "sessions/new"
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      render plain: "You have entered the correct password"
+    else
+      render plain: "You have enetered the incorrect password"
+    end
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,13 @@ Rails.application.routes.draw do
 
   get "/" => "home#index"
   post "users/login", to: "users#login"
-  resources :todos # creates seven different routes
+  resources :todos # creates seven different s
   resources :users
 
+  # get "/signin" => "sessions#new", as: :sessions
+  # post "/signin" => "sessions#create", as: :sessions
+
+  get "/signin" => "sessions#new", as: "new_sessions"
+  post "/signin" => "sessions#create", as: "sessions"
 
 end
