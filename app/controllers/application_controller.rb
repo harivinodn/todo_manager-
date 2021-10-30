3205e24d95969dc898dc251060a2aada2f99a993
harivinodn <harivinodn@gmail.com>
2021-10-30 15:08:38 +0530

    memization implemented

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,11 @@
 class ApplicationController < ActionController::Base
 
   def current_user
+    return @current_user if @current_user
+
     current_user_id = session[:current_user_id]
     if current_user_id
-      User.find(current_user_id)
+      @current_user = User.find(current_user_id)
     else
       nil
     end
