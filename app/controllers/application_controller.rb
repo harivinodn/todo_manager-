64452f31c8871b4aecdd94cf66d4f74939e13a5b
harivinodn <harivinodn@gmail.com>
2021-10-30 15:02:57 +0530

    user name is displayed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+
+  def current_user
+    current_user_id = session[:current_user_id]
+    if current_user_id
+      User.find(current_user_id)
+    else
+      nil
+    end
+  end
+
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
     @current_user_id = session[:current_user_id]
-    render "index"
+    render "index", locals: { user_name: current_user.first_name}
   end
 end
